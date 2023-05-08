22710c73851dfbcfb7d9f5453d91b788a3979fbf
admin <742679653@qq.com>
2023-05-08 09:37:04 +0800

    feat: 弹窗中图表文字

---
+++ vueDataV-master/src/components/Village/empty1.vue
@@ -0,0 +1,48 @@
+<template>
+  <div class="container">
+    <div class="chart" id="subchart"></div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "empty1",
+  mounted() {
+    this.getSubEchart();
+  },
+  methods: {
+    getSubEchart() {
+      let myChart = echarts.init(document.getElementById('subchart'));
+
+      let option = {
+        xAxis: {
+          type: 'category',
+          data: ['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun']
+        },
+        yAxis: {
+          type: 'value'
+        },
+        series: [
+          {
+            data: [120, 200, 150, 80, 70, 110, 130],
+            type: 'bar',
+            showBackground: true,
+            backgroundStyle: {
+              color: 'rgba(180, 180, 180, 0.2)'
+            }
+          }
+        ]
+      };
+      myChart.setOption(option, true);
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+.container {
+.chart {
+  height: 3rem;
+}
+}
+</style>
